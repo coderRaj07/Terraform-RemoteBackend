10fa774abc34fdf140399cb2f2d1eeb31c47e43a
Rajendra Bisoi <90323977+coderRaj07@users.noreply.github.com>
2023-10-30 15:24:58 +0530

    FEAT_0: backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "abhishek-s3-demo-xyz" # change this
+    bucket         = "s3-demo-xyz" # change this
     key            = "abhi/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
